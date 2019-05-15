8446b21def18effbdeba58c73ba8e1a868e18f01
Nguyen Hoang Thuan <nguyen.hoang.thuan@sun-asterisk.com>
2019-05-15 11:36:58 +0700

    Chapter 8: Basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by id: params[:id]
     return if @user
-    flash[:danger] = I18n.t "controllers.users_controller.user_not_exist"
+    flash[:danger] = t "controllers.users_controller.user_not_exist"
     redirect_to home_path
   end
 
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = I18n.t "controllers.users_controller.welcome"
+      flash[:success] = t "controllers.users_controller.welcome"
       redirect_to @user
     else
       render :new
