f7fc1e0ca600f60a46f90af161cce532b0620e22
Nguyen Hoang Thuan <nguyen.hoang.thuan@sun-asterisk.com>
2019-05-15 11:36:58 +0700

    Chapter 9: Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,24 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user &. authenticate params[:session][:password]
-      log_in user
-      redirect_to user
+    @user = User.find_by email: params[:session][:email].downcase
+    if @user &. authenticate params[:session][:password]
+      log_in @user
+      check_remember_me @user
+      redirect_to @user
     else
       flash.now[:danger] = t "controllers.sessions_controller.error"
       render :new
     end
   end
 
+  def check_remember_me user
+    return remember user if params[:session][:remember_me] == Settings.remember
+    forget user
+  end
+
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,12 +3,27 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
-  # order.
   fixtures :all
   include ApplicationHelper
-  # Add more helper methods to be used by all tests here...
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+  def log_in_as user
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # Log in as a particular user.
+  def log_in_as user, password: "password", remember_me: Settings.remember
+    post login_path, params:
+    {
+      session: {
+        email: user.email,
+        password: password,
+        remember_me: remember_me
+      }
+    }
   end
 end
