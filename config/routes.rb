3f9dcdae8593c821976ad105f361545e8627d795
Nguyen Hoang Thuan <nguyen.hoang.thuan@sun-asterisk.com>
2019-05-15 11:36:58 +0700

    Chapter 3: Mostly static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
     resources :microposts
     resources :users
   end
+  root "static_pages#home"
+  get "static_pages/about"
+  get "static_pages/contact"
+  get "static_pages/help"
+  get "static_pages/home"
 end
